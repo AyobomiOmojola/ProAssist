7ee08fa7c9d86d43d1ab7041d09dc7aa1fd06050
AyobomiOmojola <ayomojola12@gmail.com>
2023-10-10 15:37:42 +0100

    Update asgi.py

--- proassist/asgi.py
+++ proassist/asgi.py
@@ -3,10 +3,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path, re_path
-from chat_server.middlewares import TokenAuthMiddleWare
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proassist.settings")
+from chat_server.middlewares import TokenAuthMiddleWare
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
