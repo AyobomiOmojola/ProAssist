c5a27cdd8d96350e435f3ae0bccf95a9b6c81442
AyobomiOmojola <ayomojola12@gmail.com>
2023-10-10 16:46:35 +0100

    Update settings.py

--- proassist/settings.py
+++ proassist/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', cast=bool)
+DEBUG = os.environ.get('DEBUG')
 
 
 ALLOWED_HOSTS = ["*"]
