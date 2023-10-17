dcc68b3956816432894d46d6db8676cbdcf42dcb
AyobomiOmojola <ayomojola12@gmail.com>
2023-10-17 23:11:09 +0100

    updated

--- chat_server/routing.py
+++ chat_server/routing.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"wss/chat/(?P<id>\d+)/$", consumers.OnetoOneChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<id>\d+)/$", consumers.OnetoOneChatConsumer.as_asgi()),
 ]
