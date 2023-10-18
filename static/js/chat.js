4315c3e26bb1fafc7a9f0f0b64bfa169ba1e8d07
AyobomiOmojola <ayomojola12@gmail.com>
2023-10-18 07:19:42 +0100

    updated

--- static/js/chat.js
+++ static/js/chat.js
@@ -6,7 +6,7 @@ const receiver = JSON.parse(document.getElementById('json-otherusername').textCo
     // when running in development use this:
     // let ws_path = 'ws://'+ window.location.host + '/ws/chat/' + id;
 
-    let ws_path = 'wss://'+ window.location.host + '/wss/chat/' + id;
+    let ws_path = 'ws://'+ window.location.host + '/ws/chat/' + id;
     const parts = window.location.href.split('?')
     if (parts.length == 2){
         ws_path += '/?' + parts[1]; 
